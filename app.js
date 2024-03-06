bff90b364f47453d09cb81c416551222fd96a628
muhammadafroz7777 <muhammadafroz0320@gmail.com>
2024-03-06 10:12:32 +0500

    1

---
+++ app.js
@@ -0,0 +1,16 @@
+// // var input = document.querySelector(".input1 , .input2 , .input3 , .input4");
+// // function greetUser(){
+// //     console.log(input1.value + input2.value + input3.value + input4.value);
+// }
+
+
+
+var para = document.querySelector('.para');
+
+console.log(para.innerHTML);
+
+
+function greetUser(){
+    para.innerHTML = "Afo Awan"
+
+}
