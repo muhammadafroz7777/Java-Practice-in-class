bdf6540cf904fe8fb61aefa935d2e3312fb1bbd9
muhammadafroz7777 <muhammadafroz0320@gmail.com>
2024-03-06 10:46:38 +0500

    continue

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-// // var input = document.querySelector(".input1 , .input2 , .input3 , .input4");
-// // function greetUser(){
-// //     console.log(input1.value + input2.value + input3.value + input4.value);
-// }
+var input1 = document.querySelector(".input1");
+var input2 = document.querySelector(".input2");
+var input3 = document.querySelector(".input3");
+var input4 = document.querySelector(".input4");
+function greetUser(){
+    console.log(+input1.value + +input2.value + +input3.value + +input4.value);
+}
+
+greetUser();
 
 
 
@@ -17,11 +22,11 @@
 
 
 
-var input1 = document.querySelector('.input1');
-console.log(input1.value);
+// var input1 = document.querySelector('.input1');
+// console.log(input1.value);
 
-function greetUser(){
+// function greetUser(){
     
-    console.log(input1.value);
-    input1.value=''
-}
+//     console.log(input1.value);
+//     input1.value=''
+// }
